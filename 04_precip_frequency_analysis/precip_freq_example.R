eb8c5d2067b11d0a5d910dded83c5270d5a0d29c
David Kahler <david.m.kahler@gmail.com>
2025-02-06 16:38:58 -0500

    update to not overwrite the initial data.  TO DO: add histograms and more figures, convert to Rmd.

--- 04_precip_frequency_analysis/precip_freq_example.R
+++ 04_precip_frequency_analysis/precip_freq_example.R
@@ -3,14 +3,14 @@
 library(readr)
 x <- read_csv("04_precip_frequency_analysis/precip_freq_example_data.csv")
 
-# Use the Extreme Value Type I distribution to calculate the 5- 10- and 50-year return preriod maximum values for the 10-minute rainfall.
+# Use the Extreme Value Type I distribution to calculate the 5- 10- and 50-year return period maximum values for the 10-minute rainfall.
 m <- mean(x$Rainfall)
 s <- sd(x$Rainfall, na.rm = TRUE)
 
 a <- ((sqrt(6))*s)/(pi)
 u <- m - (0.5772 * a)
 
-Tr <- 5 # This is the return period
+Tr <- 50 # This is the return period: {5, 10, 50}
 
 y <- -log(log(Tr/(Tr-1)))
-x <- u + (a * y) # This gives the intensity
+b <- u + (a * y) # This gives the intensity
